d7efe195e00137576c92918ef805eaca43fb100f
chris <chris@trivialsec.com>
2023-02-16 09:39:58 +1100

    fix(tf): sqqs outputs

--- plans/outputs.tf
+++ plans/outputs.tf
@@ -10,3 +10,12 @@ output "trivialscan_subdomains_queue_role_arn" {
 output "trivialscan_subdomains_queue_policy_arn" {
     value = aws_iam_policy.trivialscan_subdomains_queue_policy.arn
 }
+output "subdomains_dlq_arn" {
+  value = aws_sqs_queue.trivialscan_subdomains_dlq.arn
+}
+output "subdomains_queue_arn" {
+  value = aws_sqs_queue.trivialscan_subdomains_queue.arn
+}
+output "subdomains_queue_name" {
+  value = aws_sqs_queue.trivialscan_subdomains_queue.name
+}
